5e7f0111551b73c2004c23d4db9fa68eb118985e
claudiofariacmf <claudiofariacmf@gmail.com>
2025-07-30 21:07:51 -0300

    Ajuste de hero para versión mobile

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -1,15 +1,20 @@
 import Slider from "react-slick";
 import "./Galeria.css";
 
+// Importamos las imágenes para que Vite las procese correctamente
+import img1 from "../assets/galeria/33.jpg";
+import img2 from "../assets/galeria/22.jpg";
+import img3 from "../assets/galeria/11.jpg";
+
 const Galeria = () => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
+    autoplay: true, // Para que pase solo
+    autoplaySpeed: 3000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
   };
 
   return (
@@ -17,13 +22,13 @@ const Galeria = () => {
       <h2>Galería</h2>
       <Slider {...settings}>
         <div>
-          <img src="/assets/galeria/33.jpg" alt="obra 1" />
+          <img src={img1} alt="obra 1" />
         </div>
         <div>
-          <img src="/assets/galeria/22.jpg" alt="obra 2" />
+          <img src={img2} alt="obra 2" />
         </div>
         <div>
-          <img src="/assets/galeria/11.jpg" alt="obra 3" />
+          <img src={img3} alt="obra 3" />
         </div>
       </Slider>
     </section>
